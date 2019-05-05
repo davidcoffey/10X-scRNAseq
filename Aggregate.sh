e9a6a1974845a38d5be00ffac89d97ff932d5489
David Coffey <dcoffey@fredhutch.org>
2019-05-04 23:43:24 -0700

    Added unnormalized output to aggregate.sh

--- Aggregate.sh
+++ Aggregate.sh
@@ -6,21 +6,30 @@
 # Varaibles
 #export ROOT="/fh/fast/warren_h/users/dcoffey/scRNAseq/10X041619"
 #export SAMPLESHEET_H5="$ROOT/SampleSheet/AH7273DRXX_H5_samples.csv"
-#export MATRIX_DIRECTORY="$ROOT/Aggregate/outs/filtered_feature_bc_matrix"
 
 # Start time
 START=`date +%s`
 echo Ran Aggregate.sh on `date +"%B %d, %Y at %r"`
 
+# Aggregate samples and normalize (subsample reads from higher-depth GEM wells until they all have an equal number of confidently mapped reads per cell)
 module load cellranger 
 cd $ROOT
 cellranger aggr \
---id=Aggregate \
+--id=Aggregate_normalized \
 --csv=$SAMPLESHEET_H5 \
 --normalize=mapped
 
-# convert gene expression matrix from sparse format MEX to dense format .csv
-cellranger mat2csv $MATRIX_DIRECTORY $MATRIX_DIRECTORY/Filtered_expression_matrix.csv
+# Convert gene expression matrix from sparse format MEX to dense format .csv
+cellranger mat2csv $ROOT/Aggregate_normalized/outs/filtered_feature_bc_matrix $ROOT/Aggregate_normalized/outs/filtered_feature_bc_matrix/Filtered_expression_matrix.csv
+
+# Aggregate samples and but do not normalize
+cellranger aggr \
+--id=Aggregate_unnormalized \
+--csv=$SAMPLESHEET_H5 \
+--normalize=none
+
+# Convert gene expression matrix from sparse format MEX to dense format .csv
+cellranger mat2csv $ROOT/Aggregate_unnormalized/outs/filtered_feature_bc_matrix $ROOT/Aggregate_unnormalized/outs/filtered_feature_bc_matrix/Filtered_expression_matrix.csv
 
 # End time
 END=`date +%s`
