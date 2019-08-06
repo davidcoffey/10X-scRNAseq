97c11e3b8aff696b47718ab6336b93f472968db2
davidcoffey <dcoffey@fredhutch.org>
2019-08-06 13:56:12 -0700

    Updated combined VDJ metrics

--- 10X-scRNAseq-GEX-Pipeline-MMRFINE.sh
+++ 10X-scRNAseq-GEX-Pipeline-MMRFINE.sh
@@ -35,7 +35,10 @@ AGGREGATE=$(squeue -o "%A" -h -u dcoffey -n "AGGREGATE" -S i | tr "\n" ":")
 sbatch -n 1 -c 4 -t 1-0 --job-name="MAGIC" --dependency=afterany:${AGGREGATE%?} --wrap="Rscript $ROOT/Scripts/MAGIC.R" --output=$ROOT/Logs/MAGIC.log
 
 # Combine gene expression metrics
-sbatch -n 1 -c 4 -t 1-0 --job-name="COMBINE" --dependency=afterany:${AGGREGATE%?} --wrap="Rscript $ROOT/Scripts/CombineGEXmetrics.R" --output=$ROOT/Logs/MAGIC.log
+sbatch -n 1 -c 4 -t 1-0 --job-name="COMBINE" --dependency=afterany:${AGGREGATE%?} --wrap="Rscript $ROOT/Scripts/CombineGEXmetrics.R" --output=$ROOT/Logs/GEX_metrics.log
+
+# Combine gene expression metrics
+sbatch -n 1 -c 4 -t 1-0 --job-name="COMBINE" --dependency=afterany:${AGGREGATE%?} --wrap="Rscript $ROOT/Scripts/CombineVDJmetrics.R" --output=$ROOT/Logs/VDJ_metrics.log
 
 # Create symbolic link for GEX files
 export GEX_SAMPLES="201687_6B_0 202823_6P_0 333196_6B_1 333224_6P_1 203463_5P_0 213263_5P_1 204957_7P_0 257400_7P_1 211146_18P_0 252254_18P_1 216563_22P_0 256339_22P_1 229311_28P_0 239971_39P_0 242967_37P_0 258022_44P_0 293138_60P_0 298865_28P_1 301920_37P_1 302197_72P_0 311502_77P_0 317634_79P_0 325592_60P_1 341576_79P_1 354841_44P_1 376714_72P_1 383328_39P_1 383693_77P_1"
--- CombineVDJmetrics.R
+++ CombineVDJmetrics.R
@@ -1,16 +1,16 @@
 # Combine VDJ statistics
 # David Coffey dcoffey@fredhutch.org
-# Updated May 29, 2019
+# Updated August 6, 2019
 
 library(data.table)
 library(plyr)
+library(tidyr)
+library(readxl)
 
-#path = "/Volumes/homes/Warren_FAST/users/dcoffey/scRNAseq/10X051519/Links"
-#run = "10X051519
 run = basename(Sys.getenv("ROOT"))
 path = paste(Sys.getenv("ROOT"), "Links", sep = "/")
 
-# Import V(D)J files
+# Merge all contig annotations
 files = list.files(paste(path, "All_contig_annotations", sep = "/"), full.names = TRUE)
 all.contigs = plyr::llply(files, data.table::fread, data.table = FALSE, .progress = "text")
 names(all.contigs) = gsub(x = basename(files), pattern = ".all_contig_annotations.csv", replacement = "")
@@ -18,28 +18,32 @@ all.contigs = plyr::ldply(all.contigs, data.frame, .id = "library")
 all.contigs$SampleID = gsub(all.contigs$library, pattern = "_TCR|_BCR", replacement = "")
 write.csv(all.contigs, file = paste(path, "/Combined/", run, "_all.contigs.csv", sep = ""), row.names = FALSE)
 
+# Merge clonotypes
 files = list.files(paste(path, "Clonotypes", sep = "/"), full.names = TRUE)
 clonotypes = plyr::llply(files, data.table::fread, data.table = FALSE, .progress = "text")
 names(clonotypes) = gsub(x = basename(files), pattern = ".clonotypes.csv", replacement = "")
 clonotypes = plyr::ldply(clonotypes, data.frame, .id = "library")
 clonotypes$SampleID = gsub(clonotypes$library, pattern = "_TCR|_BCR", replacement = "")
 clonotypes$receptor = ifelse(grepl(clonotypes$cdr3s_aa, pattern = "TR"), "TCR", "BCR")
-write.csv(clonotypes, file = paste(path, "/Combined/", run, "clonotypes.csv", sep = ""), row.names = FALSE)
+write.csv(clonotypes, file = paste(path, "/Combined/", run, "_clonotypes.csv", sep = ""), row.names = FALSE)
 
+# Merge consensus annotations
 files = list.files(paste(path, "Consensus_annotations", sep = "/"), full.names = TRUE)
 consensus.annotations = plyr::llply(files, data.table::fread, data.table = FALSE, .progress = "text")
 names(consensus.annotations) = gsub(x = basename(files), pattern = ".consensus_annotations.csv", replacement = "")
 consensus.annotations = plyr::ldply(consensus.annotations, data.frame, .id = "library")
 consensus.annotations$SampleID = gsub(consensus.annotations$library, pattern = "_TCR|_BCR", replacement = "")
-write.csv(consensus.annotations, file = paste(path, "/Combined/", run, "consensus_annotations.csv", sep = ""), row.names = FALSE)
+write.csv(consensus.annotations, file = paste(path, "/Combined/", run, "_consensus_annotations.csv", sep = ""), row.names = FALSE)
 
+# Merge filtered contig annotations
 files = list.files(paste(path, "Filtered_contig_annotations", sep = "/"), full.names = TRUE)
 filtered.contig.annotations = plyr::llply(files, data.table::fread, data.table = FALSE, .progress = "text")
 names(filtered.contig.annotations) = gsub(x = basename(files), pattern = ".filtered_contig_annotations.csv", replacement = "")
 filtered.contig.annotations = plyr::ldply(filtered.contig.annotations, data.frame, .id = "library")
 filtered.contig.annotations$SampleID = gsub(filtered.contig.annotations$library, pattern = "_TCR|_BCR", replacement = "")
-write.csv(filtered.contig.annotations, file = paste(path, "/Combined/", run, "filtered_contig_annotations.csv", sep = ""), row.names = FALSE)
+write.csv(filtered.contig.annotations, file = paste(path, "/Combined/", run, "_filtered_contig_annotations.csv", sep = ""), row.names = FALSE)
 
+# Merge VDJ metric summary
 files = list.files(paste(path, "Metrics_summary", sep = "/"), full.names = TRUE)
 files = files[grepl(files, pattern = "TCR|BCR")]
 metrics.summary.VDJ = plyr::llply(files, data.table::fread, data.table = FALSE, .progress = "text")
@@ -47,5 +51,23 @@ names(metrics.summary.VDJ) = gsub(x = basename(files), pattern = ".metrics_summa
 metrics.summary.VDJ = plyr::ldply(metrics.summary.VDJ, data.frame, .id = "library")
 metrics.summary.VDJ$Enrichment = ifelse(grepl(metrics.summary.VDJ$library, pattern = "TCR"), "TCR", "BCR")
 metrics.summary.VDJ$SampleID = gsub(metrics.summary.VDJ$library, pattern = "_TCR|_BCR", replacement = "")
-write.csv(metrics.summary.VDJ, file = paste(path, "/Combined/", run, "metrics_summary_VDJ.csv", sep = ""), row.names = FALSE)
+write.csv(metrics.summary.VDJ, file = paste(path, "/Combined/", run, "_metrics_summary_VDJ_by_library.csv", sep = ""), row.names = FALSE)
 
+# Create contig file of clonotypes
+clonotype.contigs = all.contigs[all.contigs$high_confidence == TRUE & 
+                                  all.contigs$is_cell == TRUE & 
+                                  all.contigs$raw_clonotype_id != "None" &
+                                  all.contigs$productive != "None" & 
+                                  all.contigs$raw_consensus_id != "None", ]
+clonotype.contigs$receptor = ifelse(grepl(clonotype.contigs$chain, pattern = "TR"), "TCR", "BCR")
+write.csv(clonotype.contigs, file = paste(path, "/Combined/", run, "_clonotype_contigs.csv", sep = ""), row.names = FALSE)
+
+# Spread CDR3 across chains by barcode and clonotype
+aggregate.chains = aggregate(data = clonotype.contigs, cdr3~library+SampleID+raw_clonotype_id+chain+barcode, paste, collapse = ",")
+aggregate.chains.count = aggregate(data = clonotype.contigs, cdr3~library+raw_clonotype_id+SampleID+chain+barcode, length)
+names(aggregate.chains.count)[6] = "multi_chain"
+aggregate.chains = merge(aggregate.chains, aggregate.chains.count)
+spread.chains = spread(aggregate.chains, chain, cdr3, fill= "")
+spread.chains$doublet = ifelse((spread.chains$IGH != "" | spread.chains$IGK != "" | spread.chains$IGL != "") &
+                                 (spread.chains$TRB != "" | spread.chains$TRA != ""), TRUE, FALSE)
+write.csv(spread.chains, file = paste(path, "/Combined/", run, "_clonotype_contigs_one_clone_per_row.csv", sep = ""), row.names = FALSE)
